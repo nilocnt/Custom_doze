938949c00c97d1ba384a6bfff668728e863e1e06
nilocnt <nilocnt@gmail.com>
2024-07-09 12:32:28 -0300

    Update service.sh

--- common/service.sh
+++ common/service.sh
@@ -16,15 +16,14 @@ done
 # Apply settings
 sleep 30s
 
-
 # Disable Fsync
 chmod 666 /sys/module/sync/parameters/fsync_enable
 chown root /sys/module/sync/parameters/fsync_enable
 echo "N" > /sys/module/sync/parameters/fsync_enable
 
-
-
 # External SD Card Speed up
 echo 'cfq' > /sys/block/mmcblk0/queue/scheduler
 echo '1024' > /sys/block/mmcblk0/queue/read_ahead_kb
 echo '1024' > /sys/block/mmcblk0/queue/nr_requests
+
+fi;
